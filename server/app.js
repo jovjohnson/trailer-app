b9fc6bf468f2a0d67ee830a7db871cae5820c731
Jovvi Johnson <jjohnsow@gmail.com>
2016-06-25 11:43:31 -0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+//COORS SUPPORT
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// app.use('/hello', function(req, res, next) {
+//   res.send('Hello World!');
+//   next();
+// });
+
+
+
+mongoose.connect('mongodb://localhost/trailerapp');
+mongoose.connection.once('open', function() {
+  console.log('*RAWM saxaphone plays*');
+  app.listen(8000);
+});
