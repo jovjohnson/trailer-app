c6ffa7718f3b84b892599b9101f1fd5bf33c3670
Jovvi Johnson <jjohnsow@gmail.com>
2016-06-25 15:13:28 -0700

    scope not working

--- client/app/scripts/controllers/movies.js
+++ client/app/scripts/controllers/movies.js
@@ -8,10 +8,18 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MoviesCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MoviesCtrl', function ($scope) {
+    $scope.movies = [
+      {
+        title: "The Empire Strikes Back",
+        url: "http://youtube.com/embed/1g3_CFmnU7k"
+      },
+      {
+        title: "Return of the Jedi",
+        url: "http://youtube.com/embed/5UfA_aKBGMc"
+      }
     ];
-  });
+
+    console.log($scope.movies);
+
+});
